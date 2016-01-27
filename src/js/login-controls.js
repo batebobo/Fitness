383157c57660bd4521c7a33da6a7743246e62daf
Boyan Vushkov <bvushkov@gmail.com>
2016-01-27 23:20:56 +0200

    Removed unused component.

--- src/js/login-controls.js
+++ src/js/login-controls.js
@@ -13,43 +13,4 @@ window.onload = function() {
         }
     });
 
-    var carousel = new Carousel();
-    carousel.init();
-    $('.inactive').hide();
-}
-
-function Carousel() {
-    var self = this;
-    self.images = ['../content/images/carousel1.jpg','../content/images/carousel2.jpg','../content/images/carousel3.jpg'];
-    self.activeIndex = 1;
-}
-
-Carousel.prototype.init = function() {
-    var container = document.createElement('div');
-    container.style.id = 'carousel-container';
-
-    var leftArrow = document.createElement('span');
-    container.appendChild(leftArrow);
-    leftArrow.style.id = 'carousel-left';
-    $(leftArrow).addClass('glyphicon glyphicon-menu-left');
-    leftArrow.style.fontSize = '1.3em';
-
-    var rightArrow = document.createElement('span');
-    container.appendChild(rightArrow);
-    rightArrow.style.id = 'carousel-right';
-    $(rightArrow).addClass('glyphicon glyphicon-menu-right');
-    rightArrow.style.fontSize = '1.3em';
-
-    for(var i = 0; i < this.images.length; i++) {
-        var currentImage = document.createElement('img');
-        currentImage.src = this.images[i];
-        currentImage.style.class = 'carousel-image'
-        container.appendChild(currentImage);
-
-        if (i != this.activeIndex) {
-            $(currentImage).hide();
-        }
-    }
-
-    $('body').append(container);
 }
